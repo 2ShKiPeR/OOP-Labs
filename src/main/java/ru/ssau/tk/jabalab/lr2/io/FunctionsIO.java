b28e83d1be18c2bf5c78404435ab18ca07a844a0
2ShKiPeR <igormokhnatov78@gmail.com>
2024-10-23 21:50:46 +0400

    added TabulatedFunctionFileReader

--- src/main/java/ru/ssau/tk/jabalab/lr2/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/jabalab/lr2/io/FunctionsIO.java
@@ -2,7 +2,11 @@ package ru.ssau.tk.jabalab.lr2.io;
 
 import ru.ssau.tk.jabalab.lr2.functions.Point;
 import ru.ssau.tk.jabalab.lr2.functions.TabulatedFunction;
+import ru.ssau.tk.jabalab.lr2.functions.factory.TabulatedFunctionFactory;
 import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -16,5 +20,23 @@ public final class FunctionsIO {
             out.printf("%f %f\n", point.x, point.y);
             out.flush();
         }
+    }
+
+    static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int size = Integer.parseInt(reader.readLine());
+        double[] xValues = new double[size];
+        double[] yValues = new double[size];
+        NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < size; i++) {
+            String line = reader.readLine();
+            String[] values = line.split(" ");
+            try {
+                xValues[i] = formatter.parse(values[0]).doubleValue();
+                yValues[i] = formatter.parse(values[1]).doubleValue();
+            } catch (ParseException e) {
+                throw new IOException(e);
+            }
+        }
+        return factory.create(xValues, yValues);
     }
 }
